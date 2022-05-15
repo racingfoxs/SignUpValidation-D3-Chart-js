22eb2d5603058417ccf84247f31202ad7d0aba6a
DR_GURUNG <racingfoxs@gmail.com>
2022-05-15 10:47:32 +0530

    commit message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Signup from "../src/components/Signup";
+import Dashboard from "../src/components/Dashboard";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+        <Routes>          
+          <Route exact path="/" element={<Signup />} />
+          <Route exact path="/Dashboard" element={<Dashboard />} />
+        </Routes>
+    </>
   );
 }
 
