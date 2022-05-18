00c6f3b236f579d1572959bfc19c568b3247bf50
DR_GURUNG <racingfoxs@gmail.com>
2022-05-18 12:23:28 +0530

    random Genetor Data completed2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,18 +1,45 @@
-import React from 'react'
-import Chart from './Chart'
+import { React, useEffect, useState } from "react";
+import Chart from "./Chart";
 
 const Dashboard = () => {
+  const testData = [
+    { id: 1, Country: "China", Population: 100 },
+    { id: 2, Country: "India", Population: 90 },
+    { id: 3, Country: "Usa", Population: 80 },
+    { id: 4, Country: "Mexico", Population: 60 },
+    { id: 5, Country: "Nepal", Population: 20 },
+    { id: 6, Country: "Sri Lanka", Population: 40 },
+  ];
+
+  const [data, setData] = useState(testData);
+
+  const randomHandler = () => {
+    const newData = data.map((ele) => {
+      ele.Population = Math.ceil(Math.random(ele.Population) * 100);
+      return ele;
+    });
+    setData(newData);
+  };
+
+  useEffect(() => {}, [data]);
 
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="container--flex --border">
-            <Chart />
-                        
+          <Chart data={data} />
+          <div className="--btn--container">
+            <button
+              className="--btn --background --btnStyle"
+              onClick={randomHandler}
+            >
+              Random Data Generator
+            </button>
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
